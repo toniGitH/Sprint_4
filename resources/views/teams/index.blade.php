2f350346764a2cfcf7a560c7dfd95cf414574d4d
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-01-07 18:55:34 +0100

    mostrando listado de equipos en la vista index

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -5,6 +5,15 @@
 @section('content')
 
     <h1>Este es el índice de la página de Equipos</h1>
+
+    <ul>
+        @foreach ($teams as $team)
+            <li>
+                {{$team->name}}
+            </li>
+        @endforeach
+    </ul>
+
     
     {{-- PAGINADOR: descomentar esto si se quiere usar el paginador --}}
     {{-- {{$teams->links()}} --}}
