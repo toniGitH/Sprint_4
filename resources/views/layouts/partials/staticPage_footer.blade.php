0ba976fff668c15042cb09c94fe28c3f7de40944
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-01-12 17:24:17 +0100

    definidas propiedades css básicas para header y footer de páginas estáticas

--- resources/views/layouts/partials/staticPage_footer.blade.php
+++ resources/views/layouts/partials/staticPage_footer.blade.php
@@ -1,3 +1,11 @@
-<footer>
-    <h2>Footer de las páginas estáticas (home, about, etc...)</h2>
+<footer class="flex justify-center bg-gray-800 text-white p-4 text-center mt-auto">
+    <div class="flex justify-between w-1/3">
+        <p class="text-white mt-2">App created by</p>
+        <div class="flex items-center justify-center mt-2">
+            <a href="https://www.linkedin.com/in/agonzalezar" class="flex items-center text-white text-lg mx-5">
+                <img src="{{ asset('img/linkedin.png') }}" alt="Enlace 2 Icon" class="w-6 h-6 mr-2">
+                Antonio González
+            </a>
+        </div>
+    </div>
 </footer>
--- resources/views/layouts/partials/staticPage_header.blade.php
+++ resources/views/layouts/partials/staticPage_header.blade.php
@@ -1,3 +1,3 @@
-<header>
+<header class="bg-orange-500 p-1 flex items-center justify-center w-full fixed top-0 z-50">
     <h1>Header de las páginas estáticas (home, about, etc...)</h1>
 </header>
