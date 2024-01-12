9c92f8ab882f185a0dcbca8b6d4e208fb0b50ef1
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-01-12 17:20:52 +0100

    generando contenedor main adaptable a altura total del viewport junto a header y footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,19 @@
 @section('title', 'Home')
 
 @section('content')
-    <h1>ESTA ES LA PÁGINA PRINCIPAL</h1>
 
-    <p>Aquí van dos cuadrados con dos imágenes que representarán estos dos enlaces:</p>
+    <main class="flex-grow flex flex-col h-full mt-24 mb-10">
+        
+        <h1>ESTA ES LA PÁGINA PRINCIPAL</h1>
 
-    <ul>
-        <li><a href="{{route('teams.index')}}">GESTIÓN DE EQUIPOS</a></li>
-        <br><br>
-        <li><a href="{{route('games.index')}}">GESTIÓN DE PARTIDOS</a></li>
-    </ul>
+        <p>Aquí van dos cuadrados con dos imágenes que representarán estos dos enlaces:</p>
+
+        <ul>
+            <li><a href="{{route('teams.index')}}">GESTIÓN DE EQUIPOS</a></li>
+            <br><br>
+            <li><a href="{{route('games.index')}}">GESTIÓN DE PARTIDOS</a></li>
+        </ul>
+        
+    </main>
+    
 @endsection
