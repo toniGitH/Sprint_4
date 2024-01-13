114e420b78af8f5223013f28cb968b045565030c
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-01-13 22:43:36 +0100

    header básico para crud de equipos

--- resources/views/layouts/partials/teamsCrudPage_header.blade.php
+++ resources/views/layouts/partials/teamsCrudPage_header.blade.php
@@ -1,3 +1,11 @@
+
 <header class="flex items-center justify-center w-full fixed top-0 z-50 bg-gray-900 text-white p-4 text-center mt-auto border-b-2 border-white">
-    <h1 class="text-white text-2xl sm:text-4xl">FOOTBALL MANAGER</h1>
+    <div class="grid grid-cols-12 w-full">
+        <h1 class="col-span-3 w-full text-xl flex items-center">FOOTBALL MANAGER</h1>
+        <h2 class="col-span-6 w-full text-4xl">GESTIÓN DE EQUIPOS</h2>
+        <div class="col-span-3 w-full teamNav flex justify-around items-center text-white">
+            <a href="{{route('home')}}"><button class="bg-slate-500 hover:bg-white hover:text-black py-1 px-2 rounded">Home</button></a>
+            <a href="{{route('teams.create')}}"><button class="bg-slate-500 hover:bg-white hover:text-black py-1 px-2 rounded">Crear Equipo</button></a>
+        </div>
+    </div>
 </header>
