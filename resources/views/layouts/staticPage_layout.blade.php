581bedae012e4c97cc718c64acf5f690fe98ba98
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-01-13 10:01:34 +0100

    corregido error de visualización del contenido. Si altura del main + altura del header + altura del footer es inferior a altura del viewport, el main crece automáticamente

--- resources/views/layouts/staticPage_layout.blade.php
+++ resources/views/layouts/staticPage_layout.blade.php
@@ -4,16 +4,17 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>@yield('title')</title>
-        {{-- @vite('resources/css/app.css') --}} {{-- Con esta línea se añade la hoja de estilos css/app --}}
-        {{-- @vite('resources/css/statics.css') --}} {{-- Con esta línea se añade la hoja de estilos css/statics --}}
+        @vite('resources/css/app.css') {{-- Con esta línea se añade la hoja de estilos css/app --}}
+        @vite('resources/css/statics.css') {{-- Con esta línea se añade la hoja de estilos css/statics --}}
         <style>
             /*             
             body{
                 background-image: url("{{ asset('img/futbol.jpg') }}"); 
             } */
+
         </style>
     </head>
-    <body>
+    <body class="min-h-screen flex flex-col ">
 
         @include('layouts.partials.staticPage_header')
 
