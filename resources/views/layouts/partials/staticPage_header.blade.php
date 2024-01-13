73dc10f62fdc3021cd6a86f7b5ab215f2deb613e
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-01-13 10:31:11 +0100

    versión inicial de header-footer y background

--- resources/views/layouts/partials/staticPage_header.blade.php
+++ resources/views/layouts/partials/staticPage_header.blade.php
@@ -1,3 +1,3 @@
-<header class="bg-orange-500 p-1 flex items-center justify-center w-full fixed top-0 z-50">
-    <h1>Header de las páginas estáticas (home, about, etc...)</h1>
+<header class="p-10 flex items-center justify-end w-full fixed top-0 z-50">
+    <h1 class="text-white text-6xl mr-10">FOOTBALL MANAGER</h1>
 </header>
--- resources/views/layouts/staticPage_layout.blade.php
+++ resources/views/layouts/staticPage_layout.blade.php
@@ -6,12 +6,13 @@
         <title>@yield('title')</title>
         @vite('resources/css/app.css') {{-- Con esta línea se añade la hoja de estilos css/app --}}
         @vite('resources/css/statics.css') {{-- Con esta línea se añade la hoja de estilos css/statics --}}
-        <style>
-            /*             
+        <style>         
             body{
-                background-image: url("{{ asset('img/futbol.jpg') }}"); 
-            } */
-
+                background-image: url("{{ asset('img/balon_bn.jpg') }}");
+                background-repeat: no-repeat; /* Evita que la imagen se repita */
+                background-size: cover; /* Hace que la imagen se adapte a toda la pantalla, manteniendo la proporción */
+                
+            }
         </style>
     </head>
     <body class="min-h-screen flex flex-col ">
