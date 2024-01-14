c738c6b6da31c7d9b067e2d082975e82ddf2e52d
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-01-14 14:14:48 +0100

    finalizados estilos del index de equipos con responsive

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -4,14 +4,13 @@
 
 @section('content')
 
-    <div CLASS="absolute bottom-20 right-10">{{$teams->links('vendor.pagination.simple-tailwind')}}</div>
-    
-    <div class="container mt-40 mx-auto w-3/4">
-        <div class="grid grid-cols-12 gap-12 text-white">
+    <div class="container pb-16 mt-32 mx-auto relative md:static md:w-3/4">
+        <div class="absolute left-1/2 transform -translate-x-1/2 bottom-5 md:bottom-20">{{$teams->links('vendor.pagination.simple-tailwind')}}</div>
+        <div class="flex flex-col  text-white items-center mb-4 md:grid grid-cols-12 gap-10 mt-14 md:mt-8">
                 @foreach ($teams as $team)
-                    <a href="{{route('teams.show', $team->id)}}" class="col-span-4 bg-black rounded-md">
+                    <a href="{{route('teams.show', $team->id)}}" class="items-center bg-black rounded-md w-2/3 md:w-full col-span-12 md:col-span-4 ">
                         <div class="text-center pt-4 pb-1 group">
-                            <p class="text-lg font-semibold">{{$team->name}}</p>
+                            <p class="font-semibold text-sm md:text-lg ">{{$team->name}}</p>
                             <p class="text-sm opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">Ver datos del equipo</p>
                         </div>
                     </a>
