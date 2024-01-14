296e0d0bffb6b6653fd117fc76d400431f2d4d7e
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-01-14 23:10:24 +0100

    eliminación de código comentado no util

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,17 +28,7 @@
                 </div>
             </a>
         </div>
-        
-        {{-- <h1>ESTA ES LA PÁGINA PRINCIPAL</h1>
 
-        <p>Aquí van dos cuadrados con dos imágenes que representarán estos dos enlaces:</p>
-
-        <ul>
-            <li><a href="{{route('teams.index')}}">GESTIÓN DE EQUIPOS</a></li>
-            <br><br>
-            <li><a href="{{route('games.index')}}">GESTIÓN DE PARTIDOS</a></li>
-        </ul> --}}
-        
     </main>
     
 @endsection
