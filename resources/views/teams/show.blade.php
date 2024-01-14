1e84388bec3dc6daa09b67ec46cd80261829f43b
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-01-14 19:14:11 +0100

    vista show de crud de equipos finalizada y con responsive

--- resources/views/teams/show.blade.php
+++ resources/views/teams/show.blade.php
@@ -4,15 +4,15 @@
 
 @section('content')
 
-    <div class="container mt-20 mx-auto">
+    <div class="container mx-auto mt-32 md:mt-20">
 
         <div class="bg-slate-200 opacity-90 w-1/2 mx-auto p-6 flex flex-col justify-center items-center mt-12 gap-y-4 rounded-lg">
-            <p class="text-xl font-bold text-slate-500">NOMBRE DEL EQUIPO</p>
-            <p class="text-3xl font-bold">{{$team->name}}</p>
-            <p class="text-xl font-bold text-slate-500">CIUDAD</p>
-            <p class="text-2xl font-bold">{{$team->city}}</p>
+            <p class="text-xl font-bold text-slate-500 text-center">NOMBRE DEL EQUIPO</p>
+            <p class="text-3xl font-bold text-center">{{$team->name}}</p>
+            <p class="text-xl font-bold text-slate-500 text-center">CIUDAD</p>
+            <p class="text-2xl font-bold text-center">{{$team->city}}</p>
             
-            <div class="mt-3 w-1/2 flex justify-between items-center mx-auto">
+            <div class="mt-3 w-full gap-4 lg:w-1/2 flex flex-col lg:flex-row justify-between items-center mx-auto">
                 <a href="{{route('teams.edit', $team)}}">
                     <button class="p-2 rounded-md transition-colors duration-300 ease-in-out bg-white hover:bg-green-500 hover:text-white">Editar equipo</button>
                 </a>
@@ -25,7 +25,7 @@
         </div>
 
         @if (session('warning'))
-            <div class="w-4/6 alert alert-info mx-auto mt-8 bg-red-500 text-center py-4 rounded-xl text-white font-semibold">
+            <div class="w-4/6 alert alert-info mx-auto mt-8 bg-red-500 text-center mb-6 py-4 rounded-xl text-white font-semibold">
                 {{ session('warning') }}
             </div>
         @endif
