04fcbe2ebea3f8dc751432cb7eed6a6b373b1169
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-01-14 18:58:39 +0100

    creada versión básica de vista show de equipos - no es responsive

--- resources/views/teams/show.blade.php
+++ resources/views/teams/show.blade.php
@@ -4,25 +4,32 @@
 
 @section('content')
 
-    <div class="container mt-20">
-        <h1>Aquí es donde aparecen los datos del equipo {{$team->name}}</h1>
-        <p><strong>Nombre: </strong>{{$team->name}}</p>
-        <p><strong>Ciudad: </strong>{{$team->city}}</p>
-    
+    <div class="container mt-20 mx-auto">
+
+        <div class="bg-slate-200 opacity-90 w-1/2 mx-auto p-6 flex flex-col justify-center items-center mt-12 gap-y-4 rounded-lg">
+            <p class="text-xl font-bold text-slate-500">NOMBRE DEL EQUIPO</p>
+            <p class="text-3xl font-bold">{{$team->name}}</p>
+            <p class="text-xl font-bold text-slate-500">CIUDAD</p>
+            <p class="text-2xl font-bold">{{$team->city}}</p>
+            
+            <div class="mt-3 w-1/2 flex justify-between items-center mx-auto">
+                <a href="{{route('teams.edit', $team)}}">
+                    <button class="p-2 rounded-md transition-colors duration-300 ease-in-out bg-white hover:bg-green-500 hover:text-white">Editar equipo</button>
+                </a>
+                <form action="{{route('teams.destroy', $team)}}" method="POST">
+                    @csrf
+                    @method('delete')
+                    <button type="submit" class="p-2 rounded-md transition-colors duration-300 ease-in-out bg-white hover:bg-red-500 hover:text-white">Eliminar equipo</button>
+                </form>
+            </div>
+        </div>
+
         @if (session('warning'))
-            <div class="alert alert-info">
+            <div class="w-4/6 alert alert-info mx-auto mt-8 bg-red-500 text-center py-4 rounded-xl text-white font-semibold">
                 {{ session('warning') }}
             </div>
         @endif
-
-        <br><br>
-        <a href="{{route('teams.edit', $team)}}"><button>Editar equipo</button></a>
-        <br><br>
-        <form action="{{route('teams.destroy', $team)}}" method="POST">
-            @csrf
-            @method('delete')
-            <button type="submit">Eliminar equipo</button>
-        </form>
+        
     </div>
     
 @endsection
