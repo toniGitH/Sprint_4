b8dedbeed8354e6b9a8e6d13840c68a657f09f6c
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-01-14 23:05:19 +0100

    cambiados los datos de los seeders de equipos y partidos

--- database/seeders/GameSeeder.php
+++ database/seeders/GameSeeder.php
@@ -21,22 +21,49 @@ class GameSeeder extends Seeder
         $game1->visitor_score = 3;
         $game1->save();
 
-        $game1 = new Game();
-        $game1->gameweek = 1;
-        $game1->date = "2024-01-01";
-        $game1->local_team_id = 3;
-        $game1->visitor_team_id = 4;
-        $game1->local_score = 0;
-        $game1->visitor_score = 0;
-        $game1->save();
+        $game2 = new Game();
+        $game2->gameweek = 1;
+        $game2->date = "2024-01-01";
+        $game2->local_team_id = 3;
+        $game2->visitor_team_id = 4;
+        $game2->local_score = 0;
+        $game2->visitor_score = 1;
+        $game2->save();
 
-        $game1 = new Game();
-        $game1->gameweek = 1;
-        $game1->date = "2024-01-01";
-        $game1->local_team_id = 5;
-        $game1->visitor_team_id = 6;
-        $game1->local_score = 0;
-        $game1->visitor_score = 0;
-        $game1->save();
+        $game3 = new Game();
+        $game3->gameweek = 1;
+        $game3->date = "2024-01-01";
+        $game3->local_team_id = 5;
+        $game3->visitor_team_id = 6;
+        $game3->local_score = 0;
+        $game3->visitor_score = 0;
+        $game3->save();
+
+        $game4 = new Game();
+        $game4->gameweek = 1;
+        $game4->date = "2024-01-01";
+        $game4->local_team_id = 7;
+        $game4->visitor_team_id = 8;
+        $game4->local_score = 1;
+        $game4->visitor_score = 3;
+        $game4->save();
+
+        $game5 = new Game();
+        $game5->gameweek = 1;
+        $game5->date = "2024-01-01";
+        $game5->local_team_id = 9;
+        $game5->visitor_team_id = 10;
+        $game5->local_score = 5;
+        $game5->visitor_score = 0;
+        $game5->save();
+
+        $game6 = new Game();
+        $game6->gameweek = 1;
+        $game6->date = "2024-01-01";
+        $game6->local_team_id = 11;
+        $game6->visitor_team_id = 12;
+        $game6->local_score = 0;
+        $game6->visitor_score = 3;
+        $game6->save();
     }
 }
--- database/seeders/TeamSeeder.php
+++ database/seeders/TeamSeeder.php
@@ -16,33 +16,63 @@ class TeamSeeder extends Seeder
         // Seeder con datos de prueba para rellenar la tabla teams de la base de datos
         // No uso factories para poder especificar datos más reales manualmente
         $team1 = new Team();
-        $team1->name = "La Merce";
-        $team1->city = "Martorell";
+        $team1->name = "New Team";
+        $team1->city = "Tokio";
         $team1->save();
 
         $team2 = new Team();
-        $team2->name = "Joan Oro";
-        $team2->city = "Martorell";
+        $team2->name = "Otomo";
+        $team2->city = "Tokio";
         $team2->save();
 
         $team3 = new Team();
-        $team3->name = "Gabriela Mistral";
-        $team3->city = "Abrera";
+        $team3->name = "Nord Ford FC";
+        $team3->city = "Osaka";
         $team3->save();
 
         $team4 = new Team();
-        $team4->name = "Salesianos";
-        $team4->city = "Abrera";
+        $team4->name = "Mambo FC";
+        $team4->city = "Osaka";
         $team4->save();
 
         $team5 = new Team();
-        $team5->name = "Trinitarios";
-        $team5->city = "Olesa";
+        $team5->name = "Azuma";
+        $team5->city = "Kyoto";
         $team5->save();
 
         $team6 = new Team();
-        $team6->name = "Severo Ochoa";
-        $team6->city = "Olesa";
+        $team6->name = "Naught boys";
+        $team6->city = "Kyoto";
         $team6->save();
+
+        $team7 = new Team();
+        $team7->name = "Magestic";
+        $team7->city = "Yokohama";
+        $team7->save();
+
+        $team8 = new Team();
+        $team8->name = "Hirado";
+        $team8->city = "Yokohama";
+        $team8->save();
+
+        $team9 = new Team();
+        $team9->name = "New Pi";
+        $team9->city = "Okinawa";
+        $team9->save();
+
+        $team10 = new Team();
+        $team10->name = "Muppet FC";
+        $team10->city = "Okinawa";
+        $team10->save();
+
+        $team11 = new Team();
+        $team11->name = "Hod Dog";
+        $team11->city = "Hokkaido";
+        $team11->save();
+
+        $team12 = new Team();
+        $team12->name = "Stampton FC";
+        $team12->city = "Hokkaido";
+        $team12->save();
     }
 }
