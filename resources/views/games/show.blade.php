444a74ce30179ad6acd63f373c53ec7f3b01ef4d
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-01-15 19:42:08 +0100

    corrección de visualización del marcador en vista show de partido

--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -9,11 +9,14 @@
         <div class="bg-slate-200 opacity-90 mx-auto p-6 flex flex-col justify-center items-center mt-12 gap-y-4 rounded-lg w-10/12 md:w-1/2">
             <p class="text-xl font-bold text-slate-500 text-center">JORNADA <span class="text-black font-bold">{{$game->gameweek}}</span></p>
             <p class="text-xl font-bold text-slate-500 text-center">FECHA <span class="text-black font-bold">{{$game->date}}</span></p>
-            <div class="flex-col md:flex-row justify-between items-center p-6 w-11/12 bg-white rounded-lg">
-                <p class="font-bold text-center w-full text-lg md:text-4xl">{{$game->local->name}} <span class="bg-gray-500 rounded-full ml-2 p-2 text-white">{{$game->local_score}}</span></p>
-                <p class="text-lg font-semibold text-center pb-1">vs</p>
-                <p class="font-bold text-center text-lg md:text-4xl">{{$game->visitor->name}} <span class="bg-gray-500 rounded-full ml-2 p-2 text-white">{{$game->visitor_score}}</span></p>
+            <div class="flex justify-center items-center bg-white rounded-lg">
+                <div class="flex-col justify-between items-center p-6 w-full">
+                    <p class="font-bold text-end w-full text-lg md:text-4xl">{{$game->local->name}} <span class="bg-gray-500 rounded-full ml-2 p-2 text-white">{{$game->local_score}}</span></p>
+                    <p class="text-lg font-semibold text-center pb-1">vs</p>
+                    <p class="font-bold text-end text-lg md:text-4xl">{{$game->visitor->name}} <span class="bg-gray-500 rounded-full ml-2 p-2 text-white">{{$game->visitor_score}}</span></p>
+                </div>
             </div>
+            
 
             <div class="mt-3 gap-4 flex flex-col lg:flex-row justify-between items-center mx-auto w-full lg:w-1/2">
                 <a href="{{route('games.edit', $game)}}">
