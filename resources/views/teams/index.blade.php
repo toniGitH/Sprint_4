db53a6c062ca5c59e7f53a2241fcc4caa196b2ea
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-01-15 21:11:54 +0100

    corrección en mensaje de equipo eliminado con éxito en el index

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -6,7 +6,7 @@
 
     <div class="container pb-16 mt-32 mx-auto relative md:static md:w-3/4">
         @if (session('warning'))
-            <div class="w-1/2 p-20 text-3xl absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 alert alert-info mx-auto bg-red-500 text-center rounded-xl text-white font-semibold z-50">
+            <div class="w-11/12 lg:w-1/2 p-20 text-3xl text-center absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 alert alert-info mx-auto bg-red-500 rounded-xl text-white font-semibold z-50">
                 {{ session('warning') }}
             </div>
         @endif
