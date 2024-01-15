44b28b1387cc0cbb7d77e9b4e378f95053e68fa7
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-01-15 12:16:26 +0100

    eliminado texto comentado no necesario

--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -35,20 +35,4 @@
         
     </div>
 
-    
-    {{-- <div style="display:flex; flex-direction:column; align-items:center; background-color: grey; margin: 10px; padding: 10px;">
-        <h1>Datos del partido</h1>
-        <h2>Jornada número: {{$game->gameweek}}</h2>
-        <span>Fecha del partido: {{$game->date}}</span>
-        <span>Jornada número: {{$game->gameweek}}</span>
-        <h3 style="font-weight:bold">{{$game->local->name}} {{$game->local_score}} - {{$game->visitor_score}} {{$game->visitor->name}} </h3>
-    </div>
-    <br><br>
-    <a href="{{route('games.edit', $game)}}"><button>Editar partido</button></a>
-    <br><br>
-    <form action="{{route('games.destroy', $game)}}" method="POST">
-        @csrf
-        @method('delete')
-        <button type="submit">Eliminar partido</button>
-    </form> --}}
 @endsection
