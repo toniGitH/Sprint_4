4e511841cad0f6ce12cae10736a7f45ad6e5bc77
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-01-15 18:42:29 +0100

    redefinida estructura básica de formulario de creación de partido - no responsive

--- resources/views/games/create.blade.php
+++ resources/views/games/create.blade.php
@@ -4,13 +4,13 @@
 
 @section('content')
     
-<div class="container mx-auto mt-32 md:mt-20">
+<div class="container mx-auto mt-32 md:mt-12">
 
-    <div class="bg-slate-200 opacity-90 w-5/6 md:w-11/12 mx-auto p-6 flex flex-col justify-center items-center mt-12 gap-y-4 rounded-lg">
+    <div class="bg-slate-200 opacity-90 w-5/6 md:w-7/12 mx-auto p-2 flex flex-col justify-center items-center mt-12 mb-2 gap-y-2 rounded-lg">
         
-        <p class="text-center text-2xl mb-4 font-bold">ALTA DE UN NUEVO PARTIDO</p>
+        <p class="text-center text-2xl mb-1 font-bold">ALTA DE UN NUEVO PARTIDO</p>
 
-        <form action="{{route('games.store')}}" method="POST" class="flex flex-col w-11/12 gap-y-6 bg-red-300">
+        <form action="{{route('games.store')}}" method="POST" class="flex flex-col items-center w-1/2 gap-y-2">
 
             {{-- Rescatamos los equipos desde el modelo Team para poder usar sus propiedades (name) en el formulario --}}
             @php
@@ -20,61 +20,86 @@
             {{-- Directiva de generación de token --}}
             @csrf
 
-            <div class="grid grid-cols-12">
+            <div class="container">
 
                 {{-- Input para la jornada y mensaje de validación --}}
-                <label for="gameweekidid" class="col-span-2">Jornada</label>
-                <select name="gameweek" id="gameweekid" class="col-span-4">
-                    <option value="">--</option>
-                    @for ($i = 1; $i <= 24; $i++)
-                        <option value="{{ $i }}" {{ old('gameweek') == $i ? 'selected' : '' }}>{{ $i }}</option>
-                    @endfor
-                </select>
-                <div class="text-red-600 col-span-6">@error('gameweek')*{{$message}}@enderror</div>
-
+                <div class="flex flex-col pb-7 relative items-start">
+                    <div class="flex">
+                        <label for="gameweekidid" class="mr-3">Jornada</label>
+                        <select name="gameweek" id="gameweekid">
+                            <option value="">--</option>
+                            @for ($i = 1; $i <= 24; $i++)
+                                <option value="{{ $i }}" {{ old('gameweek') == $i ? 'selected' : '' }}>{{ $i }}</option>
+                            @endfor
+                        </select>
+                    </div>
+                    <div class="text-red-600 absolute bottom-2 left-0">@error('gameweek')*{{$message}}@enderror</div>
+                </div>
+                
                 {{-- Input para la fecha y mensaje de validación --}}
-                <label for="dateid" class="col-span-2">Fecha</label>
-                <input type="date" name="date" value="{{ old('date') }}" class="col-span-4">
-                <div class="text-red-600 col-span-6">@error('date')*{{$message}}@enderror</div>
-
+                <div class="flex flex-col pb-7 relative items-start">
+                    <div class="flex">
+                        <label for="dateid" class="col-span-2 text-end mr-2">Fecha</label>
+                        <input type="date" name="date" value="{{ old('date') }}" class="col-span-4">
+                    </div>
+                    <div class="text-red-600 col-span-6 absolute bottom-2 left-0">@error('date')*{{$message}}@enderror</div>
+                </div>
+                
                 {{-- Input para el equipo local y mensaje de validación --}}
-                <label for="localteamid" class="col-span-2">Equipo Local</label>
-                <select name='local_team_id' id="localteamid" class="col-span-4">
-                    <option value="" {{ old('local_team_id') == "" ? 'selected' : '' }}>Selecciona el equipo local</option>
-                    @foreach ($teams as $team)
-                        <option value='{{ $team->id }}' {{ old('local_team_id') == $team->id ? 'selected' : '' }}>
-                            {{ $team->name }}
-                        </option>
-                    @endforeach
-                </select>
-                <div class="text-red-600 col-span-6">@error('local_team_id')*{{$message}}@enderror</div>
+                <div class="flex flex-col pb-7 relative items-start">
+                    <div class="flex">
+                        <label for="localteamid" class="col-span-2 text-end mr-2">Equipo Local</label>
+                        <select name='local_team_id' id="localteamid" class="col-span-4">
+                            <option value="" {{ old('local_team_id') == "" ? 'selected' : '' }}>Selecciona el equipo local</option>
+                            @foreach ($teams as $team)
+                                <option value='{{ $team->id }}' {{ old('local_team_id') == $team->id ? 'selected' : '' }}>{{ $team->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="text-red-600 col-span-6 absolute bottom-2 left-0">@error('local_team_id')*{{$message}}@enderror</div>
+                </div>
 
                 {{-- Input para la anotación local y mensaje de validación --}}
-                <label for="localscoreid" class="col-span-2">Goles</label>
-                <input type='number' name='local_score' value='{{ old("local_score") }}' min='0' max='99' placeholder='local' class="col-span-4">
-                <div class="text-red-600 col-span-6">@error('local_score')*{{$message}}@enderror</div>
+                <div class="flex flex-col pb-7 relative items-start">
+                    <div class="flex">
+                        <label for="localscoreid" class="col-span-2 text-end mr-2">Goles</label>
+                        <input type='number' name='local_score' value='{{ old("local_score") }}' min='0' max='99' placeholder='local' class="col-span-4">
+                    </div>
+                    <div class="text-red-600 col-span-6 absolute bottom-2 left-0">@error('local_score')*{{$message}}@enderror</div>
+                </div>
 
                 {{-- Input para el equipo visitante y mensaje de validación --}}
-                <label for="visitorteamid" class="col-span-2">Equipo Visitante</label>
-                <select name='visitor_team_id' id="visitorteamid" class="col-span-4">
-                    <option value="" {{ old('visitor_team_id') == "" ? 'selected' : '' }}>Selecciona el equipo visitante</option>
-                    @foreach ($teams as $team)
-                        <option value='{{ $team->id }}' {{ old('visitor_team_id') == $team->id ? 'selected' : '' }}>
-                            {{ $team->name }}
-                        </option>
-                    @endforeach
-                </select>
-                <div class="text-red-600 col-span-6">@error('visitor_team_id')*{{$message}}@enderror</div>
+                <div class="flex flex-col pb-7 relative items-start">
+                    <div class="flex">
+                        <label for="visitorteamid" class="col-span-2 text-end mr-2">Equipo Visitante</label>
+                        <select name='visitor_team_id' id="visitorteamid" class="col-span-4">
+                            <option value="" {{ old('visitor_team_id') == "" ? 'selected' : '' }}>Selecciona el equipo visitante</option>
+                            @foreach ($teams as $team)
+                                <option value='{{ $team->id }}' {{ old('visitor_team_id') == $team->id ? 'selected' : '' }}>{{ $team->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="text-red-600 col-span-6 absolute bottom-2 left-0">@error('visitor_team_id')*{{$message}}@enderror</div>
+                </div>
 
                 {{-- Input para la anotación visitante y mensaje de validación --}}
-                <label for="visitorscoreid" class="col-span-2">Goles</label>
-                <input type='number' name='visitor_score' value='{{ old("visitor_score") }}' min='0' max='99' placeholder='visit' class="col-span-4">
-                <div class="text-red-600 col-span-6">@error('visitor_score')*{{$message}}@enderror</div>
+                <div class="flex flex-col pb-7 relative items-start">
+                    <div class="flex">
+                        <label for="visitorscoreid" class="col-span-2 text-end mr-2">Goles</label>
+                        <input type='number' name='visitor_score' value='{{ old("visitor_score") }}' min='0' max='99' placeholder='visit' class="col-span-4">
+                    </div>
+                    <div class="text-red-600 col-span-6 absolute bottom-2 left-0">@error('visitor_score')*{{$message}}@enderror</div>
+                </div>
+    
             </div>
 
             {{-- Botón de tipo submit para enviar el formulario --}}
-            <button type="submit">GUARDAR</button>
+            <button type="submit" class="w-3/4 lg:w-2/3 mx-auto p-2 rounded-md transition-colors duration-300 ease-in-out bg-white hover:bg-green-500 hover:text-white">CREAR PARTIDO</button>
 
         </form>
+
+    </div>
+
+</div>
 
 @endsection
