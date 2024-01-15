df95d961f29ed3a79bfbd6092beb07993e978bf1
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-01-15 07:17:26 +0100

    esteblecido fondo, header y footer para layout de partidos

--- resources/views/layouts/gamesCrudPage_layout.blade.php
+++ resources/views/layouts/gamesCrudPage_layout.blade.php
@@ -6,6 +6,14 @@
         <title>@yield('title')</title>
         @vite('resources/css/app.css') {{-- Con esta línea se añade la hoja de estilos de css/app --}}
         @vite('resources/css/cruds.css') {{-- Con esta línea se añade la hoja de estilos css/cruds --}}
+        <style>         
+            body{
+                background-image: url("{{ asset('img/balon_bn.jpg') }}");
+                background-repeat: no-repeat; /* Evita que la imagen se repita */
+                background-size: cover; /* Hace que la imagen se adapte a toda la pantalla, manteniendo la proporción */
+                
+            }
+        </style>
     </head>
     <body class="min-h-screen flex flex-col ">
 
